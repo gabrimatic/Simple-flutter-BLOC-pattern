942542a81ddab5ec7065005ae9fcf3f37af51bbe
mahmoud-eslami <eslamimahmoud98@gmail.com>
2021-02-25 01:11:28 +0330

    bloc observer added to sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_flutter_bloc/utils/bloc_observer/bloc_observer.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
+  // call observer for observe all cubit changes
+  Bloc.observer = MyBlocObserver();
+  
   runApp(SampleBlocApp());
 }
